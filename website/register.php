bb2e8c478a7d8ff4d1209ace76add8f60fd8928d
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2016-08-27 15:05:29 -0300

    Fix links on footer

--- website/register.php
+++ website/register.php
@@ -97,20 +97,26 @@
     <!-- FOOTER SECTION -->
     <div class="row footer text-center vertical-align">
         <div class="col-sm-4">
-            <img src="images/pin.png" class="img-responsive center-block img-footer" alt="Pino no mapa">
-            <address>
-                <strong>Universidade Federal do Paraná</strong><br>
-                Centro Politécnico<br>
-                Curitiba, PR
-            </address>
+            <a href="http://maps.google.com/?q=-25.449753,-49.232942" target="_blank">
+                <img src="images/pin.png" class="img-responsive center-block img-footer" alt="Pino no mapa">
+                <address>
+                    <strong>Universidade Federal do Paraná</strong><br>
+                    Centro Politécnico<br>
+                    Curitiba, PR
+                </address>
+            </a>
         </div>
         <div class="col-sm-4">
-            <img src="images/envelope.png" class="img-responsive center-block img-footer" alt="Contato email">
-            <a href="mailto:pmantovani94@gmail.com">pmantovani94@gmail.com</a>
+            <a href="mailto:pmantovani94@gmail.com">
+                <img src="images/envelope.png" class="img-responsive center-block img-footer" alt="Contato email">
+                <p>pmantovani94@gmail.com</ap>
+            </a>
         </div>
         <div class="col-sm-4">
-            <img src="images/telephone.png" class="img-responsive center-block img-footer" alt="Telefone">
-            <a href="tel:+554191477772">+55 41 9147-7772</a>
+            <a href="tel:+554191477772">
+                <img src="images/telephone.png" class="img-responsive center-block img-footer" alt="Telefone">
+                <p>+55 41 9147-7772</p>
+            </a>
         </div>
     </div>
 </body>
