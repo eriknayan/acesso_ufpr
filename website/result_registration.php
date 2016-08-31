e1098c433600e4a16448482daf6aab737e5fd5d5
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2016-08-30 21:56:27 -0300

    Add basic structure for MySQL insert

--- website/result_registration.php
+++ website/result_registration.php
@@ -14,6 +14,34 @@
     <meta name="description" content="O sistema de acesso oficial da UFPR">
 </head>
 <body>
+<?php
+    //$dbhost = 'localhost:3036';
+    $dbhost = 'localhost';
+    $dbuser = 'adminsql';
+    $dbpass = '!acessoufpr_sql16';
+    $dbname = 'acessoufpr';
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+    if(! $conn ) {
+        die('Could not connect: ' . mysql_error());
+    }
+
+    echo 'Connected successfully';
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+
+    $query = "INSERT INTO Users (name,email) VALUES ('$name','$email')";
+    $retval = mysqli_query($conn, $query);
+
+    if (! $retval) {
+        die('Query failed: ' . mysql_error());
+    }
+
+    mysqli_close($conn);
+
+?>
+
     Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
 </body>
