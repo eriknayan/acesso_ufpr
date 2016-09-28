2e9388d086113730ca9a5e74b574a767e9831c03
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2016-09-27 21:18:02 -0300

    New database schema

--- mysql/script.sql
+++ mysql/script.sql
@@ -1,55 +1,70 @@
-/*  
-
-Script de criação de Base de Dados - Acesso UFPR 
-
+/*
+Script de criação de Base de Dados - Acesso UFPR
 Erik Nayan & Pedro Mantovani
-
-Versão 1.0 - 10 set 2016 
-
 */
 
+-- DROP DATABASE arion; -- USE WITH EXTRA CAREFUL
 CREATE DATABASE IF NOT EXISTS arion;
 
 USE arion;
 
-CREATE TABLE IF NOT EXISTS users (
-         cardID    	  BIGINT(12) UNSIGNED NOT NULL,
-         name         VARCHAR(50) NOT NULL,
-         email        VARCHAR(50) NOT NULL,
-         password	  VARCHAR(20) NOT NULL,
-         grr          INT(8) UNSIGNED NOT NULL,
-         type         ENUM('Estudante','Professor','Servidor') NOT NULL,
-         regdate	  DATE DEFAULT '2017-01-01' NOT NULL,
-         status		  BIT DEFAULT 1 NOT NULL, -- 1=Ativo, 0=Inativo;
-         expiration	  DATE DEFAULT '2100-01-01' NOT NULL,
-         PRIMARY KEY  (cardID)
+CREATE TABLE IF NOT EXISTS Users (
+    cardId      BIGINT(12) UNSIGNED NOT NULL,
+    name        VARCHAR(50) NOT NULL,
+    email       VARCHAR(50) NOT NULL,
+    password    VARCHAR(20) NOT NULL,
+    grr         INT(8) UNSIGNED NOT NULL,
+    type        ENUM('Estudante','Professor','Servidor') NOT NULL,
+    regdate	    DATE DEFAULT '2017-01-01' NOT NULL,
+    status      BIT DEFAULT 1 NOT NULL, -- 1=Ativo, 0=Inativo;
+    expiration  DATE DEFAULT '2100-01-01' NOT NULL,
+    balance     DECIMAL(6,2) NOT NULL,
+    PRIMARY KEY (cardID)
+);
+
+CREATE TABLE IF NOT EXISTS Restaurants (
+    restId    INT NOT NULL,
+    restName  VARCHAR(50) NOT NULL,
+    restAddr  VARCHAR(250) NOT NULL,
+    PRIMARY KEY (restId)
+);
+
+CREATE TABLE IF NOT EXISTS Recharges (
+    recId       INT NOT NULL,
+    cardId      BIGINT(12) UNSIGNED NOT NULL,
+    value       DECIMAL(6,2) NOT NULL,
+    rectime     TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY (recId),
+    FOREIGN KEY (cardId)
+        REFERENCES Users(cardId)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Transactions (
+    tranId      BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    cardID      BIGINT(12) UNSIGNED NOT NULL,
+    value       DECIMAL(6,2) NOT NULL,
+    trantime    TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    restId      INT NOT NULL,
+    PRIMARY KEY (tranId),
+    FOREIGN KEY (restId)
+        REFERENCES Restaurants(restId)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
  );
 
-CREATE TABLE IF NOT EXISTS clients (
-		 cardID    	  BIGINT(12) UNSIGNED NOT NULL,
-		 balance	  DECIMAL(6,2) NOT NULL,
-		 status		  BIT DEFAULT 1 NOT NULL, -- 1=Ativo, 0=Inativo;
-		 PRIMARY KEY  (cardID)
- );
+CREATE USER IF NOT EXISTS 'read'@'%' IDENTIFIED BY '***PASSWD***'; -- read access only, '%' guarantees access from any computer
+GRANT SELECT ON arion.Users to 'read'@'%';
+GRANT SELECT ON arion.Transactions to 'read'@'%';
+GRANT SELECT ON arion.Recharges to 'read'@'%';
+GRANT SELECT ON arion.Restaurants to 'read'@'%';
 
-CREATE TABLE IF NOT EXISTS transactions (
-		 id 		  BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-		 cardID	      BIGINT(12) UNSIGNED NOT NULL,
-		 value        DECIMAL(2,2) NOT NULL,
-		 type		  BIT NOT NULL, -- 1=Crédito, 0=Desconto;
-		 ttime		  TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-		 campus		  ENUM('Politecnico','Botanico','Agrarias','Reitoria') NOT NULL,
-		 PRIMARY KEY  (id)
- );
+CREATE USER IF NOT EXISTS 'form'@'%' IDENTIFIED BY '***PASSWD***'; -- db access for new user manipulation
+GRANT SELECT, INSERT, UPDATE ON arion.Users to 'form'@'%';
 
-CREATE USER IF NOT EXISTS 'scan'@'%' IDENTIFIED BY 'scan@acesso_ufpr16!'; -- o scan pode acessar de qualquer máquina a DB
-
-GRANT SELECT, INSERT, UPDATE ON arion.transactions to 'scan'@'%'; -- acesso somente a tabela apropriada
-
-GRANT SELECT, INSERT, UPDATE ON arion.clients to 'scan'@'%'; -- acesso somente a tabela apropriada
-
-CREATE USER IF NOT EXISTS 'form'@'%' IDENTIFIED BY 'form@acesso_ufpr16!'; -- o form pode acessar de qualquer máquina a DB
-
-GRANT SELECT, INSERT, UPDATE ON arion.users to 'form'@'%'; -- acesso somente a tabela apropriada
+CREATE USER IF NOT EXISTS 'scanner'@'%' IDENTIFIED BY '***PASSWD***'; -- inserts new transactions in db
+GRANT SELECT, INSERT ON arion.Transaction to 'scanner'@'%';
+GRANT SELECT ON arion.Users to 'scanner'@'%';
+GRANT SELECT ON arion.Restaurants to 'scanner'@'%';
 
 FLUSH PRIVILEGES;
