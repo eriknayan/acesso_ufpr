5be2377a8dbe10c7818c49ca4b022ebac01fd5d0
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2016-10-19 12:19:50 -0200

    Partial back-end functionality for login

--- website/login.php
+++ website/login.php
@@ -1,40 +1,103 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <script src="js/jquery-3.1.0.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Acesso RU UFPR - Cadastro</title>
-    <meta name="description" content="O sistema de acesso oficial da UFPR">
-</head>
-<body>
-    <div class="container">
-        <!-- HEADER SECTION -->
-        <?php include("header.php"); ?>
-        <!-- LOGIN FIELDS -->
-        <div class="row form-margin">
-            <div class="col-sm-4 col-sm-offset-4">
-                <form role="form" method="post">
-                    <div class="form-group">
-                        <label for="emailInput">Endereço de Email</label>
-                        <input type="email" name="name" class="form-control" id="emailInput" placeholder="Digite seu email" autofocus>
-                    </div>
-                    <div class="form-group">
-                        <label for="passwordInput">Senha</label>
-                        <input type="password" name="passwd" class="form-control" id="passwordInput" placeholder="Senha">
-                    </div>
-                    <div class="checkbox">
-                        <label><input type="checkbox">Lembre-se do usuário</label>
-                    </div>
-                    <button type="submit" class="btn btn-default">Login</button>
-                </form>
-            </div>
-        </div>
-    <!-- FOOTER SECTION -->
-    <?php include("footer.php") ?>
-</body>
+<?php
+
+function showErrorMessage($msg) {
+    $_SESSION['Error'] = $msg;
+    include('login_page.php');
+    die();
+}
+
+// Checks if a given user name is in the database
+// Returns true if one or more entries were found, false otherwise
+function userInDb($email) {
+    $dbhost = 'localhost';
+//$dbhost = 'arion.ddns.net';
+    $dbuser = 'form';
+    $dbpass = '***PASSWD***';
+    $dbname = 'arion';
+    $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+    // Checks if successfully connected to db
+    if($conn->connect_errno) {
+        showErrorMessage("Nosso sistema está com dificuldades técnicas no momento. 
+        Por favor, tente novamente mais tarde.");
+    }
+
+    $email = $conn->real_escape_string($email);
+    $checkQuery = "SELECT * FROM Users WHERE name='$email';";
+    $resultCursor = $conn->query($checkQuery);
+    if ($resultCursor >= 1) {
+        $resultCursor->close();
+        $conn->close();
+        return true;
+    }
+    $resultCursor->close();
+    $conn->close();
+    return false;
+}
+
+// Validates a cookie received from a client.
+// Returns true if cookie is valid, false otherwise
+function validateCookie($cookie, $secretKey) {
+
+    $splitCookie = explode("|", $cookie);
+    $numElements = count($splitCookie);
+    // Parse number of elements in cookie
+    if ($numElements != 2) {
+        return false;
+    }
+
+    // Check if user is in db
+    if (!userInDb($splitCookie[0])) {
+        return false;
+    }
+
+    // Check if cookie hash is correct
+    if (hash_hmac("sha256", $splitCookie[0], $secretKey) === $splitCookie[1]) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function createSecureCookie($email, $secretKey) {
+    return $email . "|" . hash_hmac("sha256", $email, $secretKey);
+}
+
+if($_SERVER['REQUEST_METHOD'] == "GET") {
+    // Shows login page in case no data was posted to server
+    include("login_page.php");
+    die();
+}
+else {
+
+    // In case post to site didn't contain email, password or cookie, show error message
+    if ((empty($_POST["email"]) || empty($_POST["passwd"])) && !isset($_COOKIE["sess"])) {
+        showErrorMessage("Email ou senha não foram fornecidos.");
+    }
+
+    $secretKey = "***SECRET***";
+
+    // Validate by cookie
+    if (isset($_COOKIE["sess"])) {
+        if (validateCookie($_COOKIE["sess"], $secretKey)) {
+            // Valid cookie, go to welcome page
+            die();
+        }
+        else {
+            // Invalid cookie
+            showErrorMessage("Erro no login, por favor tente novamente.");
+        }
+    }
+
+    if (isset($_POST["remember"])) {
+        // Expires in 60 days
+        // TODO: Change secure flag to true after HTTPS is implemented
+        setcookie("sess", createSecureCookie($email, $secretKey), time()+60*60*24*60, "/", "arion.ddns.net", false, true);
+
+    }
+
+}
+
+
+?>
---
+++ website/login_page.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.1.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Acesso RU UFPR - Cadastro</title>
+    <meta name="description" content="O sistema de acesso oficial da UFPR">
+</head>
+<body>
+<div class="container">
+    <!-- HEADER SECTION -->
+    <?php include("header.php"); ?>
+    <!-- LOGIN FIELDS -->
+    <div class="row form-margin">
+        <div class="col-sm-4 col-sm-offset-4">
+            <form role="form" method="post">
+                <div class="form-group">
+                    <label for="emailInput">Endereço de Email</label>
+                    <input type="email" name="email" class="form-control" id="emailInput" placeholder="Digite seu email" autofocus>
+                </div>
+                <div class="form-group">
+                    <label for="passwordInput">Senha</label>
+                    <input type="password" name="passwd" class="form-control" id="passwordInput" placeholder="Senha">
+                </div>
+                <div class="checkbox">
+                    <label><input type="checkbox" name="remember" value="checked">Lembre-se do usuário</label>
+                </div>
+                <button type="submit" class="btn btn-default">Login</button>
+            </form>
+        </div>
+    </div>
+    <!-- FOOTER SECTION -->
+    <?php include("footer.php") ?>
+</body>
