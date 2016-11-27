bb7931f73ee9352db6e266c039865320766b345c
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2016-11-27 21:04:59 -0200

    Add front-end for email confirmation

--- website/confirmation.php
+++ website/confirmation.php
@@ -74,7 +74,35 @@
     $conn->commit();
     $conn->close();
 
-    echo "Hi " . $row["name"] . "!\n";
-    echo "Thank you! Your account was activated successfully!";
+    header("refresh:5;url=login.php");
 ?>
 
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.1.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Acesso RU UFPR - Confirmação</title>
+    <meta name="description" content="O sistema de acesso oficial da UFPR">
+</head>
+<body>
+    <div class="container">
+        <!-- HEADER SECTION -->
+        <?php include("header.php"); ?>
+
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="alert alert-success">
+                    Obrigado! Sua conta foi ativada com sucesso!<br>
+                    Você será redirecionado em 5 segundos.
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
