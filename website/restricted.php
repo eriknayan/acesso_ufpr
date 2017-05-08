7645c2f14fba3728a530183b8fff2fda6e3a58c1
Erik Nayan <eriknayan@gmail.com>
2017-05-07 22:20:20 -0300

    Restricted area created

--- website/restricted.php
+++ website/restricted.php
@@ -1,48 +1,3 @@
-<?php
-
-session_start();
-
-require_once("utilities.php");
-require_once("db_operations.php");
-
-if (!isset($_COOKIE["session"])) {
-    // Redirect to login page in case there are no session cookies
-    header("Location: login.php");
-}
-if (!validateCookie($_COOKIE["session"])) {
-    // Redirect to login page in case the session cookie is invalid
-    header("Location: login.php?logout=true");
-}
-
-    $db = new DBOperator();
-    $info = $db->getUserInfoFromSessionCookie($_COOKIE["session"]);
-
-    if (!$info) {
-        die();
-    }
-    else {
-        // Puts info in session variables
-        $_SESSION["name"] = $info["name"];
-        $_SESSION["email"] = $info["email"];
-        $_SESSION["role"] = $info["type"];
-        $_SESSION["grr"] = $info["grr"];
-        $_SESSION["cardId"] = $info["cardId"];
-        $_SESSION["balance"] = $info["balance"];
-    }
-
-    if (!isset($_SESSION["transactions"])) {
-        $transactions = $db->getLastFiveTransactions($_COOKIE["session"]);
-        if (!$transactions) {
-            die();
-        }
-        else {
-            $_SESSION["transactions"] = $transactions;
-        }
-    }
-?>
-
-
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -52,65 +7,27 @@ if (!validateCookie($_COOKIE["session"])) {
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
     <link rel="stylesheet" href="css/welcome.css">
-    <title>Acesso RU UFPR - Início</title>
+    <title>Acesso RU UFPR - Área Restrita</title>
     <meta name="description" content="O sistema de acesso oficial da UFPR">
 </head>
 <body>
     <div class="container">
         <!-- HEADER SECTION -->
         <?php include("header.php"); ?>
-        <!-- MENU SECTION -->
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#">Arion</a>
-                </div>
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Saldo</a></li>
-                    <li><a href="/recharge.php">Recarga</a></li>
-                    <li><a href="#">Transações</a></li>
-                    <li><a href="/personalinfo.php">Informações pessoais</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="login.php?logout=true" class="navbar-left"><img src="images/logout.svg" class="logout-img">Sair</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-        <!-- BALANCE SECTION -->
-        <div class="row text-center">
-            <div class="col-xs-12">
-                <h3>Bem-vindo <?php echo $_SESSION["name"] ?>!</h3>
-                <h3 class="section-margin">Seu saldo é de: <?php echo 'R$' . $_SESSION["balance"]; ?></h3>
-                <a href="/recharge.php">Faça uma recarga aqui</a>
-                <h3 class="section-margin">Suas últimas 5 transações foram:</h3>
-                <table class="table table-hover text-center">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Data e Hora</th>
-                            <th>Valor</th>
-                            <th>Local</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        // Fill up table dynamically
-                        while ($row = $_SESSION["transactions"]->fetch_array()) {
-                            echo "<tr>";
-                            echo "<td>" . $row["tranId"] . "</td>";
-                            echo "<td>" . $row["tranTime"] . "</td>";
-                            if ($row["type"]) { echo "<td>" . $row["value"] . "</td>"; }
-                            else { echo "<td>-" . $row["value"] . "</td>"; }
-                            echo "<td>" . $row["restName"] . "</td>";
-                            echo "</tr>";
-                        }
-                        ?>
-                    </tbody>
-                </table>
-                <a href="#">Veja seu histórico completo aqui</a>
-            </div>
+        <!-- READER SECTION -->
+        <div class="barcode-reader text-center">
+            <h1 class="text-uppercase sub-title"> Leitura de carteirinha</h1>
+            <img src="images/barcode.svg" class="img-responsive center-block img-feature" alt="Barcode" height="320" width="320">
+            <p></p>
+            <p class="lead">Clique no botão abaixo para realizar a leitura pelo <i>app</i>.</p>
+            <p><a class="btn btn-lg btn-success" href="http://zxing.appspot.com/scan?ret=http://arionufpr.ddns.net/restricted.php?codigo={CODE}" role="button">Ler carteirinha</a></p>
+        <!--  CÓDIGO DA CARTEIRINHA -->
+            <div class="form-group has-feedback">
+                <label for="codInput"></label>
+                <input type="text" name="cod" class="form-control" id="codInput" placeholder="Código da carteirinha" value="<?= $_GET['codigo'] ?>" />
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                <div class="help-block with-errors"></div>
+            </div>           
         </div>
     </div>
 </body>
