c9d2cf2902d1284072d400028f2869d8b18e5909
Erik Nayan <eriknayan@gmail.com>
2017-05-19 14:15:45 -0300

    <a> now is white; use btn-std for grey buttons texts;

--- website/footer.php
+++ website/footer.php
@@ -1,7 +1,7 @@
 <!-- FOOTER SECTION -->
     <div class="row footer text-center">
         <div class="col-md-4">
-            <a href="http://maps.google.com/?q=-25.449753,-49.232942" target="_blank">
+            <a class="btn-std" href="http://maps.google.com/?q=-25.449753,-49.232942" target="_blank">
                 <img src="images/pin.png" class="img-responsive center-block margin-20" alt="Pino no mapa">
                 <address>
                     <strong>Universidade Federal do Paraná</strong><br>
@@ -11,13 +11,13 @@
             </a>
         </div>
         <div class="col-md-4">
-            <a href="mailto:arion.ufpr@gmail.com">
+            <a class="btn-std" href="mailto:arion.ufpr@gmail.com">
                 <img src="images/envelope.png" class="img-responsive center-block margin-20" alt="Contato email">
                 <p>arion.ufpr@gmail.com</p>
             </a>
         </div>
         <div class="col-md-4">
-            <a href="tel:+5541991477772">
+            <a class="btn-std" href="tel:+5541991477772">
                 <img src="images/telephone.png" class="img-responsive center-block margin-20" alt="Telefone">
                 <p>+55 41 9147-7772</p>
             </a>
